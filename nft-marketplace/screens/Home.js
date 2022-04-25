5388a1535b9c1b2ff7823bc5f963c33745432e9e
Yonathan  Admasu <75287408+Yonathan-Admasu728@users.noreply.github.com>
2022-04-25 00:03:58 -0700

    initialized react-natve app, fleshed out the main components and screens fetched data from NFTDATA

---
+++ nft-marketplace/screens/Home.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { View, SafeAreaView, FlatList, Text } from "react-native";
+
+import { COLORS, NFTData } from "../constants";
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList 
+            data={NFTData}
+            renderItem={({ item }) =><Text>{item.name}</Text> }
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default Home
